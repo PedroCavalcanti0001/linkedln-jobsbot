e8da895281ff2700073cf14e33624e232e784f01
pedro <pedroeugeniocavalcanti@gmail.com>
2022-09-05 11:44:42 -0300

    Classe com os metodos do bot do telegram movida para pacote proprio.

---
+++ src/main/java/me/pedroeugenio/linkedlnjobsbot/telegram/TelegramBot.java
@@ -0,0 +1,40 @@
+package me.pedroeugenio.linkedlnjobsbot.telegram;
+
+import com.pengrad.telegrambot.request.SendMessage;
+import com.pengrad.telegrambot.response.SendResponse;
+import me.pedroeugenio.linkedlnjobsbot.models.Job;
+
+import java.util.List;
+
+public class TelegramBot {
+
+    private StringBuilder getStringBuilder(List<Job> jobs){
+        StringBuilder stringBuilder = new StringBuilder();
+        stringBuilder.append("Vagas encontradas ({quantidade}):".replace("{quantidade}", String.valueOf(jobs.size())));
+        stringBuilder.append("\n\n");
+        return stringBuilder;
+    }
+
+    private StringBuilder addJobsToStringBuilder(List<Job> jobs){
+        StringBuilder stringBuilder = getStringBuilder(jobs);
+        for (Job job : jobs) {
+            String template = TelegramConstants.TEMPLATE_MESSAGE.replace("{empresa}", job.getCompany())
+                    .replace("{vaga}", job.getTitle())
+                    .replace("{strTime}", job.getStrTime())
+                    .replace("{loc}", job.getLocation())
+                    .replace("{link}", job.getShortLink());
+            stringBuilder.append(template);
+        }
+        return stringBuilder;
+    }
+
+    public void sendJobsMessage(List<Job> jobs) {
+        TelegramConstants.LOGGER.info("Enviando vagas para o telegram");
+        StringBuilder jobsAsStr = addJobsToStringBuilder(jobs);
+        SendResponse response = TelegramConstants.BOT.execute(new SendMessage(TelegramConstants.CHAT, jobsAsStr.toString()));
+        if(response.isOk())
+            TelegramConstants.LOGGER.info("Vagas enviadas com sucesso");
+        else
+            TelegramConstants.LOGGER.error("Houve um erro ao enviar -> ".concat(String.valueOf(response.errorCode())));
+    }
+}
