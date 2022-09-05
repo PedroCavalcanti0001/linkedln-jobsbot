5699d6d944322e90ea835cdc6f6d38ad13667a46
pedro <pedroeugeniocavalcanti@gmail.com>
2022-09-05 15:20:58 -0300

    Icones adicionados ao template da mensagem

--- src/main/java/me/pedroeugenio/linkedlnjobsbot/telegram/TelegramConstants.java
+++ src/main/java/me/pedroeugenio/linkedlnjobsbot/telegram/TelegramConstants.java
@@ -8,7 +8,7 @@ public class TelegramConstants {
     protected static final String TOKEN = System.getenv("LBOT_TELEGRAM_TOKEN");
     protected static final com.pengrad.telegrambot.TelegramBot BOT = new com.pengrad.telegrambot.TelegramBot(TOKEN);
     protected static final Long CHAT = -603392344L;
-    protected static final String TEMPLATE_MESSAGE = "Vaga: ".concat("{vaga}").concat("\n").concat("Empresa: ").concat("{empresa}")
-            .concat("\n").concat("Postada ").concat("{strTime}").concat("\n").concat("Localização: ").concat("{loc}")
-            .concat("\n").concat("Link: ").concat("{link}").concat("\n\n");
+    protected static final String TEMPLATE_MESSAGE = "\uD83D\uDCBB Vaga: ".concat("{vaga}").concat("\n").concat("\uD83C\uDFE6 Empresa: ").concat("{empresa}")
+            .concat("\n").concat("⏱ Postada ").concat("{strTime}").concat("\n").concat("📌 Localização: ").concat("{loc}")
+            .concat("\n").concat("\uD83D\uDC49 Link: ").concat("{link}").concat("Uma nova busca será realizada as ").concat("{tempoNovaBusca}").concat("\n\n");
 }
