eb052dedf01f8927dcae7b2d12f55a8fab00f833
pedro <pedroeugeniocavalcanti@gmail.com>
2022-09-08 09:55:14 -0300

    Configuração de id do canal do bot transferidas para arquivo configuração.yml

--- src/main/java/me/pedroeugenio/linkedlnjobsbot/telegram/TelegramConstants.java
+++ src/main/java/me/pedroeugenio/linkedlnjobsbot/telegram/TelegramConstants.java
@@ -8,8 +8,9 @@ public class TelegramConstants {
     protected static final Logger LOGGER = LogManager.getLogger(TelegramBot.class.getName());
     protected static final String TOKEN = AppConfig.load().getBotToken();
     protected static final com.pengrad.telegrambot.TelegramBot BOT = new com.pengrad.telegrambot.TelegramBot(TOKEN);
-    protected static final Long CHAT = -603392344L;
-    protected static final String TEMPLATE_MESSAGE = "\uD83D\uDCBB Vaga: ".concat("{vaga}").concat("\n").concat("\uD83C\uDFE6 Empresa: ").concat("{empresa}")
-            .concat("\n").concat("⏱ Postada ").concat("{strTime}").concat("\n").concat("📌 Localização: ").concat("{loc}")
-            .concat("\n").concat("\uD83D\uDC49 Link: ").concat("{link}").concat("Uma nova busca será realizada as ").concat("{tempoNovaBusca}").concat("\n\n");
+    protected static final Long CHAT = AppConfig.load().getBotChannel();
+    protected static final String TEMPLATE_MESSAGE = "\uD83D\uDCBB Vaga: ".concat("{vaga}")
+            .concat("\n").concat("\uD83C\uDFE6 Empresa: ").concat("{empresa}").concat("\n").concat("⏱ Postada ")
+            .concat("{strTime}").concat("\n").concat("📌 Localização: ").concat("{loc}").concat("\n")
+            .concat("\uD83D\uDC49 Link: ").concat("{link}").concat("\n\n");
 }
