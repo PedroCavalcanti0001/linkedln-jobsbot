c6887bf2e6795a20a51324a605e0f36556533b40
pedro <pedroeugeniocavalcanti@gmail.com>
2022-09-09 13:44:32 -0300

    metodo newFile atualizado para contemplar utf-8

--- src/main/java/me/pedroeugenio/linkedlnjobsbot/utils/FileUtils.java
+++ src/main/java/me/pedroeugenio/linkedlnjobsbot/utils/FileUtils.java
@@ -1,12 +1,13 @@
 package me.pedroeugenio.linkedlnjobsbot.utils;
 
-import java.io.FileWriter;
 import java.io.IOException;
+import java.io.PrintWriter;
+import java.nio.charset.StandardCharsets;
 
 public class FileUtils {
 
     public static void newFile(String path, String content) throws IOException {
-        FileWriter fileWriter = new FileWriter(path);
+        PrintWriter fileWriter = new PrintWriter(path, StandardCharsets.UTF_8.name());
         fileWriter.write(content);
         fileWriter.close();
     }
