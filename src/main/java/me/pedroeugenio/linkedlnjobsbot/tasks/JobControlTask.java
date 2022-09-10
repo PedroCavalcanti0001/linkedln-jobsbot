65d271980bcda4dfc4554d5348e4831398dadf26
pedro <pedroeugeniocavalcanti@gmail.com>
2022-09-09 23:05:34 -0300

    Tarefa para verificar os jobs no repositorio criada

--- src/main/java/me/pedroeugenio/linkedlnjobsbot/tasks/JobControlTask.java
+++ src/main/java/me/pedroeugenio/linkedlnjobsbot/tasks/JobControlTask.java
@@ -10,20 +10,47 @@ import org.apache.log4j.LogManager;
 import org.apache.log4j.Logger;
 
 import java.time.Duration;
+import java.util.List;
 import java.util.Optional;
+import java.util.Timer;
+import java.util.TimerTask;
+import java.util.concurrent.TimeUnit;
 
 public class JobControlTask {
     private static final Logger LOGGER = LogManager.getLogger(JobControlTask.class.getName());
     private static final JobRepository JOB_REPOSITORY = new JobRepositoryMemImpl();
 
+    public JobControlTask() {
+        checkTask();
+    }
+
+
     public void verify(Job job) throws JobAlreadySubmittedException {
         Optional<Job> jobById = JOB_REPOSITORY.findJobById(job.getJobId());
         if (jobById.isPresent() && jobById.get().getJobId() != 0L) {
-            if (canRemove(job))
-                JOB_REPOSITORY.remove(job.getJobId());
+            LOGGER.info(String.valueOf(jobById.get()).concat(" já havia sido enviada antes."));
             throw new JobAlreadySubmittedException();
         } else
             JOB_REPOSITORY.add(job);
+    }
+
+    private void checkTask() {
+        TimerTask timerTask = new TimerTask() {
+            @Override
+            public void run() {
+                List<Job> all = JOB_REPOSITORY.findAll();
+                checkRepositoryJobList(all);
+            }
+        };
+        Timer timer = new Timer("linkedlnjobsbot-timer2");
+        timer.scheduleAtFixedRate(timerTask, 0, TimeUnit.MINUTES.toMillis(15));
+    }
+
+    private void checkRepositoryJobList(List<Job> jobList) {
+        for (Job job : jobList) {
+            if (canRemove(job))
+                JOB_REPOSITORY.remove(job.getJobId());
+        }
     }
 
     private boolean canRemove(Job job) {
