647c7975f7c6e56cd2c519c4b0f2b96460481a5f
Pedro Cavalcanti <pedroeugeniocavalcanti@gmail.com>
2024-04-02 12:43:20 -0300

    fix: headers

--- src/main/java/me/pedroeugenio/linkedlnjobsbot/scraper/JobsConstants.java
+++ src/main/java/me/pedroeugenio/linkedlnjobsbot/scraper/JobsConstants.java
@@ -15,20 +15,20 @@ class JobsConstants {
     protected static final Logger LOGGER = LogManager.getLogger(JobsSearch.class.getName());
     protected final static String BASE_URL = "http://www.linkedin.com/jobs/search/?currentJobId=1";
     protected final static Map<String, String> HEADERS = new HashMap<String, String>() {{
+        put("accept", "text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.7");
         put("accept-language", "pt-BR,pt;q=0.9,en;q=0.8,en-GB;q=0.7,en-US;q=0.6");
-        put("accept-encoding", "gzip, deflate, br");
-        put("accept", "text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8,application/" +
-                "signed-exchange;v=b3;q=0.9");
-        put("pragma", "no-cache");
-        put("sec-ch-ua", "\"Chromium\";v=\"104\", \" Not A;Brand\";v=\"99\", \"Microsoft Edge\";v=\"104\"");
+        put("^cookie", "bcookie=^\\^v=2");
+        put("^cache-control", "max-age=0");
+        put("^sec-ch-ua", "^\\^Microsoft");
         put("sec-ch-ua-mobile", "?0");
-        put("sec-ch-ua-platform", "Windows");
+        put("^sec-ch-ua-platform", "Windows");
         put("sec-fetch-dest", "document");
         put("sec-fetch-mode", "navigate");
-        put("sec-fetch-site", "same-origin");
+        put("sec-fetch-site", "none");
         put("sec-fetch-user", "?1");
         put("upgrade-insecure-requests", "1");
+        put("user-agent", "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/123.0.0.0 Safari/537.36 Edg/123.0.0.0");
     }};
-    protected final static String USER_AGENT = "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/104.0.5112.102 Safari/537.36 Edg/104.0.1293.70";
+    protected final static String USER_AGENT = "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/123.0.0.0 Safari/537.36 Edg/123.0.0.0";
     protected static final SortEnum SORT = SortEnum.valueOf(PROPERTIES.getOrder().toUpperCase());
 }
